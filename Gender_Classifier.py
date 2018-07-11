76a7dfef95ed1252bb8faca66abbc36b14d2804d
Shantanu Garg <shantanu.garg@student.ie.edu>
2018-07-11 19:10:57 +0200

    Final Version: Changed the training dataset

--- Gender_Classifier.py
+++ Gender_Classifier.py
@@ -1,3 +1,6 @@
+
+
+
 from sklearn import tree
 from sklearn import ensemble
 from sklearn import neighbors
@@ -8,45 +11,46 @@ from sklearn.metrics import accuracy_score
 #X = [height, weight, shoe size]
 #Training Datset
 
-X = [[190,80,10],[132,45,6],[189,67,9],[202,100,13],[145,67,6],[154,45,8],[148,89,9],[159,90,7],[188,78,12],[163,80,8]]
-Y =['Female','Male','Male','Male','Female','Female','Male','Female','Female','Male']
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],[177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+
 
 
 #Test Dataset
 _X=[[184,84,44],[198,92,48],[183,83,44],[166,47,36],[170,60,38],[172,64,39],[182,80,42],[180,80,43]]
 _Y=['male','male','male','female','female','female','male','male']
 
-# Classifying using Decision Trees
+#Classification
 clf = tree.DecisionTreeClassifier()
-
-clf = clf.fit(X,Y)
-
-prediction = clf.predict(_X)
-
-print("Decision tree Result:",accuracy_score(_Y, prediction))
-
-
-#Classification using Random Forest
-
 clf2 = ensemble.RandomForestClassifier()
-
-clf2 = clf2.fit(X,Y)
-prediction2 = clf2.predict(_X)
-print("Random Forest Result:",accuracy_score(_Y, prediction2))
-
-#Classification using KNN
-
 clf3 = neighbors.KNeighborsClassifier()
-clf3 = clf3.fit(X,Y)
-
-prediction3 = clf3.predict(_X)
-print("KNN Result:",accuracy_score(_Y,prediction3))
-
-#Classification using neural network
-
 clf4 = neural_network.MLPClassifier()
+
+#Training
+clf = clf.fit(X,Y)
+clf2 = clf2.fit(X,Y)
+clf3 = clf3.fit(X,Y)
 clf4 = clf4.fit(X,Y)
 
+#Prediction
+
+prediction = clf.predict(_X)
+prediction2 = clf2.predict(_X)
+prediction3 = clf3.predict(_X)
 prediction4 = clf4.predict(_X)
+
+#Results
+
+print("Decision tree Result:",accuracy_score(_Y, prediction))
+print("Random Forest Result:",accuracy_score(_Y, prediction2))
+print("KNN Result:",accuracy_score(_Y,prediction3))
 print("Neural Network Result:",accuracy_score(_Y,prediction4))
+
+
+
+
+
+
+
+
 
